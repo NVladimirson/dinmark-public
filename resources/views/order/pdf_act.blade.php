2ca995b2399a243247b40adefd75d2e8158ab519
polmain <polmain8@gmail.com>
2020-09-24 15:28:45 +0300

    Fix act pdf head color

--- resources/views/order/pdf_act.blade.php
+++ resources/views/order/pdf_act.blade.php
@@ -100,11 +100,12 @@
 </p>
 
 <table class="f8 border">
-    <tr style="background-color: #ffffff">
-        <td colspan="4">За даними Товариство з обмеженою відповідальністю "Леомарк", грн </td>
-        <td colspan="4">За даними {{$company->name}}, грн </td>
-    </tr>
+
     <tbody>
+    <tr>
+        <td colspan="4" style="background-color: #ffffff">За даними Товариство з обмеженою відповідальністю "Леомарк", грн </td>
+        <td colspan="4" style="background-color: #ffffff">За даними {{$company->name}}, грн </td>
+    </tr>
     <tr class="f-b t-c">
         <td>Дата</td>
         <td>Документ</td>
